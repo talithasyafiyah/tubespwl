cafe1c1e41740a210d6e9de7dbaec1ec6a76761c
talithasyafiyah <talithasyafiyah1112@gmail.com>
2022-05-01 15:20:35 +0700

    second

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title') haha</title>
+    <title>@yield('title')</title>
 
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 

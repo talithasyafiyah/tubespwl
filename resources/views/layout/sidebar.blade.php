905b4e5209f3868bb2fd2065f3e3d6953fa1178d
talithasyafiyah <talithasyafiyah1112@gmail.com>
2022-06-04 21:44:19 +0700

    talit

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -3,9 +3,6 @@
             <div class="sidebar-wrapper active">
                 <div class="sidebar-header">
                     <div class="d-flex justify-content-between">
-                        <div class="logo">
-                            <a href="/dashAdmin"><img src="" width="100" alt="Logo" srcset=""></a>
-                        </div>
                         <div class="toggler">
                             <a href="#" class="sidebar-hide d-xl-none d-block"><i class="bi bi-x bi-middle"></i></a>
                         </div>
--- resources/views/siswa/tabungan.blade.php
+++ resources/views/siswa/tabungan.blade.php
@@ -9,13 +9,13 @@
                 <div class="page-title">
                     <div class="row">
                         <div class="col-12 col-md-6 order-md-1 order-last">
-                            <h3>Drugs</h3>
+                            <h3>Menabung</h3>
                         </div>
                         <div class="col-12 col-md-6 order-md-2 order-first">
                             <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
                                 <ol class="breadcrumb">
                                     <li class="breadcrumb-item"><a href="index.php">Dashboard</a></li>
-                                    <li class="breadcrumb-item active" aria-current="page">Drugs</li>
+                                    <li class="breadcrumb-item active" aria-current="page">Menabung</li>
                                 </ol>
                             </nav>
                         </div>
@@ -23,27 +23,40 @@
                 </div>
                 <section class="section">
                     <div class="card">
-                        <div class="card-body">
-                            <table class="table table-striped" id="table1">
-                                <thead>
-                                    <tr>
-                                        <th>No</th>
-                                        <th>Drug Name</th>
-                                        <th>Generic Name</th>
-                                        <th>Brand Name</th>
-                                        <th></th>
-                                        <th></th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                </tbody>
-                            </table>
+                        <div class="card-content">
+                            <div class="card-body">
+                                <form class="form form-horizontal">
+                                    <div class="form-body">
+                                        <div class="row">
+                                            <div class="col-md-4">
+                                                <label>Nama</label>
+                                            </div>
+                                            <div class="col-md-8 form-group">
+                                                <input type="text" id="first-name" class="form-control"
+                                                    name="fname" placeholder="Nama">
+                                            </div>
+                                            <div class="col-md-4">
+                                                <label>Kelas</label>
+                                            </div>
+                                            <div class="col-md-8 form-group">
+                                                <input type="email" id="email-id" class="form-control"
+                                                    name="email-id" placeholder="Kelas">
+                                            </div>
+                                            <div class="col-md-4">
+                                                <label>Saldo</label>
+                                            </div>
+                                            <div class="col-md-8 form-group">
+                                                <input type="number" id="contact-info" class="form-control"
+                                                    name="contact" placeholder="Saldo">
+                                            </div>
+                                            <div class="col-sm-12 d-flex justify-content-end">
+                                                <button type="submit"
+                                                    class="btn btn-primary me-1 mb-1"><i class="fa-solid fa-hand-holding-dollar pe-4"></i>Menabung</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
                         </div>
                     </div>
                 </section>
